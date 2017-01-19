6473e3d6529a7cd05817f7795589fafbfb627716
Mauro <mauro.bonfietti@gmail.com>
2017-01-19 14:54:31 -0300

    Get profile of one facebook user, using the Facebook API Graph.

--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,30 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    $name = $request->getAttribute('name');
+    if (!$name) {
+        return $this->renderer->render($response, 'index.phtml', $args);
+    }
+
+    $fb = new Facebook\Facebook([
+        'default_graph_version' => 'v2.8',
+        'app_id' => 'X',
+        'app_secret' => 'Y',
+        'default_access_token' => 'Z',
+    ]);
+
+    try {
+        $response = $fb->get("/$name?fields=id,first_name,last_name");
+    } catch (Facebook\Exceptions\FacebookResponseException $e) {
+        echo 'Graph returned an error: ' . $e->getMessage();
+        exit;
+    } catch (Facebook\Exceptions\FacebookSDKException $e) {
+        echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        exit;
+    }
+
+    $user = $response->getGraphUser();
+
+    return json_encode(json_decode($user), JSON_PRETTY_PRINT);
+
 });

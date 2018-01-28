5c7863411bda5de2394a230e3386a6e39bca5d65
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-01-28 18:02:17 -0300

    Deleted empty file middleware.php

--- tests/Functional/BaseTestCase.php
+++ tests/Functional/BaseTestCase.php
@@ -7,21 +7,8 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use Slim\Http\Environment;
 
-/**
- * This is an example class that shows how you could set up a method that
- * runs the application. Note that it doesn't cover all use-cases and is
- * tuned to the specifics of this skeleton app, so if your needs are
- * different, you'll need to change it.
- */
 class BaseTestCase extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * Use middleware when running application?
-     *
-     * @var bool
-     */
-    protected $withMiddleware = true;
-
     /**
      * Process the application given a request method and URI
      *
@@ -32,7 +19,6 @@ class BaseTestCase extends \PHPUnit_Framework_TestCase
      */
     public function runApp($requestMethod, $requestUri, $requestData = null)
     {
-        // Create a mock environment for testing with
         $environment = Environment::mock(
             [
                 'REQUEST_METHOD' => $requestMethod,
@@ -40,42 +26,27 @@ class BaseTestCase extends \PHPUnit_Framework_TestCase
             ]
         );
 
-        // Set up a request object based on the environment
         $request = Request::createFromEnvironment($environment);
 
-        // Add request data, if it exists
         if (isset($requestData)) {
             $request = $request->withParsedBody($requestData);
         }
 
-        // Set up a response object
         $response = new Response();
 
-        // Use DotEnv config file
         $dotenv = new \Dotenv\Dotenv(__DIR__.'/../../');
         $dotenv->load();
 
-        // Use the application settings
         $settings = require __DIR__ . '/../../src/settings.php';
 
-        // Instantiate the application
         $app = new App($settings);
 
-        // Set up dependencies
         require __DIR__ . '/../../src/dependencies.php';
 
-        // Register middleware
-        if ($this->withMiddleware) {
-            require __DIR__ . '/../../src/middleware.php';
-        }
-
-        // Register routes
         require __DIR__ . '/../../src/routes.php';
 
-        // Process the application
         $response = $app->process($request, $response);
 
-        // Return the response
         return $response;
     }
 }
